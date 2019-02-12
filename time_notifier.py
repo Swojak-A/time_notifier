4ffee48a4599c991787331c2c635497ed80bab71
kaoss <swojak.a@gmail.com>
2019-02-12 18:48:53 +0100

    add notify method

--- time_notifier.py
+++ time_notifier.py
@@ -1,20 +1,25 @@
 import datetime
 from time import sleep
+from pygame import mixer
 
 class TimeNotifier:
     def __init__(self):
         pass
 
-    def time_count(self):
+    def time_count(self, func):
         while True:
             time_check = datetime.datetime.now()
             print("Present time: {}, {}, {}".format(time_check.hour, time_check.minute, time_check.second))
-            if time_check.minute == 0:
-                print("New hour")
+            if time_check.minute == 00:
+                func
                 sleep(60)
             sleep(30)
 
+    def notify(self):
+        mixer.init()
+        mixer.music.load("mp3/test.mp3")
+        mixer.music.play()
 
 if __name__ == "__main__":
     t = TimeNotifier()
-    t.time_count()
+    t.time_count(t.notify())
