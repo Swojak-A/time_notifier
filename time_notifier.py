a3f18607453faf576aef396303abdbdbc51f1e3a
kaoss <swojak.a@gmail.com>
2019-02-14 16:43:28 +0100

    add notify method

--- time_notifier.py
+++ time_notifier.py
@@ -9,15 +9,24 @@ class TimeNotifier:
     def time_count(self, func):
         while True:
             time_check = datetime.datetime.now()
-            print("Present time: {}, {}, {}".format(time_check.hour, time_check.minute, time_check.second))
-            if time_check.minute == 00:
+            print("Present time: {}:{}:{}".format(time_check.hour, time_check.minute, time_check.second))
+            if time_check.minute == 40:
                 func
                 sleep(60)
             sleep(30)
 
-    def notify(self):
+    def notify(self, folder="mp3/", lang="eng-emma/", hour=None):
+        if hour == None:
+            file = "test.mp3"
+        else:
+            file = "{}.mp3".format(str(hour))
+
+        path = "{}{}{}".format(folder, lang, file)
+
+        print(path)
+
         mixer.init()
-        mixer.music.load("mp3/test.mp3")
+        mixer.music.load(path)
         mixer.music.play()
 
 if __name__ == "__main__":
