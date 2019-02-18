7d3104d3ef8a9dbaaed5321f0fd2318d94840dbf
kaoss <swojak.a@gmail.com>
2019-02-18 19:12:37 +0100

    add working in background / console window opt

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
 from distutils.core import setup
 import py2exe
 
-mp3_files = [('mp3/eng-emma', ['mp3/eng-emma/test.mp3'])]
+import os
+
+mp3_files = []
+mp3_dir = "mp3"
+for item1 in os.listdir(mp3_dir):
+    full_lang_dir_path = "{}/{}".format(mp3_dir, item1)
+    if os.path.isdir(full_lang_dir_path):
+        for item2 in os.listdir(full_lang_dir_path):
+            full_file_path = "{}/{}/{}".format(mp3_dir, item1, item2)
+            if os.path.isfile(full_file_path):
+                input = full_lang_dir_path, [full_file_path]
+                mp3_files.append(input)
 
 setup(
-    console=['time_notifier.py'],
+    # windows=['time_notifier.py'], #uncomment to work in background
+    console=['time_notifier.py'], #uncomment to work in console window
     data_files = mp3_files,
     options={
                 "py2exe":{
